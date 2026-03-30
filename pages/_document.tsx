1fa476aa24a477b40c3f10e8f45bc52fe31de057
DailyWellFact <dailyhabitfact@gmail.com>
2026-03-30 15:19:27 +0300

    Remove Google Fonts stylesheet link
    Removed Google Fonts link from the document head.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
