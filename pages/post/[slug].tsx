8d2b0860012ec9294add64336f50fe0b7385f709
DailyWellFact <dailyhabitfact@gmail.com>
2026-04-02 09:12:47 +0300

    Remove unnecessary newline in [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -385,7 +385,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: false,
   };
 };
-
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == "post" && slug.current == $slug][0]{
     _id,
